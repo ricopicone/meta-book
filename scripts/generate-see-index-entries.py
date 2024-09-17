922afff7627fc0c93f878faec70ede6f2be7dac6
Rico Picone <rpicone@stmartin.edu>
2024-09-16 21:28:29 -0700

    Untested but should generate the see index entries from a json file without building in the dictionary

--- scripts/generate-see-index-entries.py
+++ scripts/generate-see-index-entries.py
@@ -1,15 +1,21 @@
+import json
+import argparse
+
+argparser = argparse.ArgumentParser(description="Generate index entries for see entries")
+argparser.add_argument("equivalents", help="Path to the JSON file containing the see entries (scripts/see-index-entries.json)")
+argparser.add_argument("tex_output", help="Path to the TeX file to write the index entries to (scripts/see-index-entries.tex)")
+args = argparser.parse_args()
+
 def generate_index(equivalents):
     index_entries = ""
     for key, value in equivalents.items():
         for item in value:
-            index_entries += "\\myindex{" + item + "|see{" + key + "}}\n"
+            index_entries += "\\index{" + item + "|see{" + key + "}}\n"
     return index_entries
 
-equivalents = {
-    # "Amplifers": ["Amplifires am I right"],
-}
+equivalents = json.load(open(args.equivalents))
 
 index = generate_index(equivalents)
 
-with open("index-see-entries.tex", "w") as f:
+with open(args.tex_output, "w") as f:
     f.write(index)
